d8477e61e1c46e931f920a45d45016115a4e0b27
turkandagdeviren <turkandagdeviren@yahoo.com>
2022-04-12 00:09:50 +0200

    day38: Inheritance - Method Overriding

--- src/day38_Inheritance/carTask/BMW.java
+++ src/day38_Inheritance/carTask/BMW.java
@@ -1,4 +1,24 @@
 package day38_Inheritance.carTask;
 
-public class BMW {
+public class BMW extends Car{
+
+
+    public BMW(String model, String color, int price, int miles) {
+        super("BMW", model, color, price, miles);
+    }
+
+    public void breaksDown(){
+        System.out.println(brand+" "+model+" breaks down every 100 miles");
+    }
+
+    public void racing(){
+        System.out.println(brand+ " "+ model+ " is a racing car");
+    }
+
 }
+/*
+2. BMW:
+					extra methods:
+						breaksDown()
+						racing()
+ */
--- src/day38_Inheritance/carTask/Tesla.java
+++ src/day38_Inheritance/carTask/Tesla.java
@@ -1,5 +1,13 @@
 package day38_Inheritance.carTask;
 
-public class Tesla {
+public class Tesla extends Car {
+
+    public Tesla(String model, String color, int price, int miles) {
+        super("Tesla", model, color, price, miles);
+    }
+
+    public void autoPilot(){
+        System.out.println(brand+" "+model+ " in autopilot mode");
+    }
 
 }
