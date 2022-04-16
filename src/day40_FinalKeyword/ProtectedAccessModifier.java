b32107223957957e226871258b92f084f7a93709
turkandagdeviren <turkandagdeviren@yahoo.com>
2022-04-16 23:47:15 +0200

    day40

--- src/day40_FinalKeyword/ProtectedAccessModifier.java
+++ src/day40_FinalKeyword/ProtectedAccessModifier.java
@@ -2,10 +2,20 @@ package day40_FinalKeyword;
 
 public class ProtectedAccessModifier {
 
+    //always visible within the same package
+    // visible outside the package in subclass ONLY
+
     static String name1= "Cydeo"; //default access modifier. it can be inherited only to the classes in the same package. static variables can be called by class name in the same package
     protected static String name2 ="WoodenSpoon"; //protected. it could be visible if the class is subclass
 
 
+    static void method1(){
+
+    }
+
+    protected static void method2(){
+
+    }
 
 
 
--- src/utilities/AccessModifiersTest4.java
+++ src/utilities/AccessModifiersTest4.java
@@ -1,7 +1,17 @@
 package utilities;
 
-public class AccessModifiersTest4 {
+import day40_FinalKeyword.ProtectedAccessModifier;
+
+public class AccessModifiersTest4 extends ProtectedAccessModifier {
     public static void main(String[] args) {
+
+       // System.out.println(AccessModifiersTest4.name1);//default variable is not visible despite it is subclass
+        System.out.println(AccessModifiersTest4.name2); // since the class is subclass protected variable became visible
+
+        System.out.println(ProtectedAccessModifier.name2);
+
+      //  AccessModifiersTest4.method1(); default method can not be visible despite subclass
+        AccessModifiersTest4.method2();// protected method can be visible when it is subclass
 
 
 
