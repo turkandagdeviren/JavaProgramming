4ea80e4ba26d0f4ec53d4035799b6c7e104f7b67
turkandagdeviren <turkandagdeviren@yahoo.com>
2022-04-17 14:45:35 +0200

    day40

---
+++ src/day40_FinalKeyword/Animal.java
@@ -0,0 +1,58 @@
+package day40_FinalKeyword;
+
+public class Animal {
+
+    private String name;
+    private final String breed; // final ekleyerek unchangeable yaptik
+    private final char gender;
+    private final String color;
+    private String size;
+    private int age;
+
+    public Animal(String name, String breed, char gender, String color, String size, int age) {
+        this.name = name;
+        this.breed = breed;
+        this.gender = gender;
+        this.color = color;
+        this.size = size;
+        this.age = age;
+    }
+
+    //final variable kullandigimizda private variable lar icin setter create edemeyiz cunku verileri yeniden set edemeyiz
+    // we can set only the unfinal variables
+    //private datalar final ise setter kullanamadigimiz icin bi condition
+    //we can read the final variables but can not modify them
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    //------------------------setters-------------------------------
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+
+
+}
---
+++ src/day40_FinalKeyword/FinalVariable.java
@@ -0,0 +1,46 @@
+package day40_FinalKeyword;
+
+public class FinalVariable {
+    //instance variables can not be initialized by final variable. because we use instances to create object
+    //to get rid of compile error we create constructor for instance variables; static block for static variables
+
+    final String birthDay;
+    final static String name;
+
+    static{
+        name="Batch 25";
+    }
+
+    public FinalVariable(String birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public static void main(String[] args) {
+        //final variables can not be reassigned
+        double pi= 3.14;
+      //  pi= 4.14; default oldugu icin reassign yapabildik
+
+        final String name;
+        name= "Java";
+
+      //  name="Wooden spoon"; it can not be reaasigned. it gives compile error
+        System.out.println(name);
+
+        System.out.println("--------------------------------");
+
+        FinalVariable obj= new FinalVariable("May/01");
+        System.out.println(obj.birthDay);
+
+       // obj.birthDay= "June/01"; // final variables can not be reassigned
+
+
+        System.out.println("--------------------------------");
+
+        System.out.println(FinalVariable.name); // static variables are called by class name
+
+        // FinalVariable.name="Python";  // final variable can not be reassigned
+
+
+
+    }
+}
