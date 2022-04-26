e622b3c8796b12fdd7d65f08bcb2f4a4c8aabfd1
turkandagdeviren <turkandagdeviren@yahoo.com>
2022-04-26 23:40:59 +0200

    day 42

---
+++ src/day42_Exceptions/ThrowKeyword.java
@@ -0,0 +1,37 @@
+package day42_Exceptions;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ThrowKeyword {
+    // it creates exception, it doesnt handle exception. it is used for manually creating exception
+    // for ex to terminate program
+
+    public static void main(String[] args) {
+
+        System.out.println("enter your age:"); // -20
+        Scanner scan= new Scanner(System.in);
+        int age= scan.nextInt();
+
+        if(age<0){
+         /*   System.err.println("invalid age: "+ age);
+            System.exit(1);  */
+
+            throw new InputMismatchException("Age can not be negative: "+ age); // exception classindan yeni bir object create ettigimiz icin new kelimesini kullaniyoruz
+        }
+
+        if(age>21){
+            System.out.println("you are eligible");
+        }else{
+            throw new RuntimeException("you must be at least 21 years old");
+        }
+
+
+    }
+
+
+
+
+
+
+}
--- src/day42_Exceptions/ThrowKeyword2.java
+++ src/day42_Exceptions/ThrowKeyword2.java
@@ -1,13 +1,18 @@
 package day42_Exceptions;
 
+import java.util.NoSuchElementException;
+
 public class ThrowKeyword2 {
+    public static void main(String[] args) {
 
-    public static void main(String[] args) throws ArithmeticException {
 
-        System.out.println("test started");
-        System.out.println(8/0);
-        System.out.println("test completed");
+        throw new NoSuchElementException("There is no such element as break");
 
+      //  throw new Rectangle(5,7);  it gives compile error because it is not exception object
+
+      //  System.out.println("hello world"); -> once we use the throw keyword compiler knows there is an exception (line 11) we can not write new fragment codes
+
+      //throw new InterruptedException(); -> we can not use the throw keyword with checked exception
 
 
 
