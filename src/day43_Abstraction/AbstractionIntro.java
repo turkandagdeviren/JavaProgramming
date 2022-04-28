bedd413eb4b133371176e708684eda1d65f18f84
turkandagdeviren <turkandagdeviren@yahoo.com>
2022-04-28 17:12:24 +0200

    day 43

---
+++ src/day43_Abstraction/AbstractionIntro.java
@@ -0,0 +1,25 @@
+package day43_Abstraction;
+
+public class AbstractionIntro {
+
+    /*
+• process of hiding implementation details from the user
+• Only the functionality will be provided to the user
+• Focusing on the essential qualities of something rather than one specific
+example. (Ignoring the irrelevant & unimportant)
+• User will have the information on what the object does instead of how it does
+
+abstract method:
+• A method that does not have body, only signature
+• A method that’s meant to be overridden
+• Abstract keyword is used to create abstract method.
+abstract method can not be static, final, can not have private access modifier
+abstract method can only be created in an abstract class or interface
+abstract class should be parent
+
+     */
+
+
+
+
+}
---
+++ src/day43_Abstraction/car/Car.java
@@ -0,0 +1,58 @@
+package day43_Abstraction.car;
+
+public abstract class Car {
+
+    private final String make, model;
+    private String color;
+    private final int year;
+    private double price;
+
+    public Car(String make, String model, String color, int year, double price) {
+        this.make = make;
+        this.model = model;
+        setColor(color);
+        if(year<1886){
+            throw new RuntimeException("invalid year: "+ year);
+        }
+        this.year = year; // setter i olmadigi icin eger bi condition olacaksa sadece constructorda konulabilir. ve condition oncesinde yazilir ki sarta uymuyorsa hic set edilmesin
+
+        if(price<=0){
+            throw new RuntimeException("invalid price");
+        }
+        setPrice(price);
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void stop(){
+        System.out.println("press the brake");
+    }
+    public abstract void start();
+
+}
